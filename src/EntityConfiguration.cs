b34c262734cdc96bfcdbb01a34bdd49e226434c4
restlessmedia <phillycooper@hotmail.com>
2019-03-13 17:14:00 +0000

    Added configuration

---
+++ src/EntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace restlessmedia.Module.Data.EF
+{
+  public abstract class EntityConfiguration<T> : EntityTypeConfiguration<T>
+    where T : Entity
+  {
+    public EntityConfiguration()
+    {
+      Property(x => x.EntityGuid).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+      Ignore(x => x.EntityId);
+      Ignore(x => x.EntityType);
+    }
+  }
+}
---
+++ src/LicensedEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace restlessmedia.Module.Data.EF
+{
+  public class LicensedEntityConfiguration<T> : EntityConfiguration<T>
+    where T : LicensedEntity
+  {
+    public LicensedEntityConfiguration()
+      : base()
+    {
+      Property(x => x.LicenseId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+    }
+  }
+}
